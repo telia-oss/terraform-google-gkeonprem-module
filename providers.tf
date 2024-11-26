6dfe1bbb38b38f05b97d8b3cce5de4c459faf4d7
SadriG91 <sadri.gargouri@teliacompany.com>
2024-11-26 17:08:21 +0100

    Update provider versions to use more flexible version constraints

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.12.0"
+      version = "~> 6.12"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.33.0"
+      version = "~> 2.33"
     }
   }
   required_version = ">= 1.7.0"
