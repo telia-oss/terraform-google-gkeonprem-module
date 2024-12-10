63810898f2269991737edc92b4507493268ec494
SadriG91 <sadri.gargouri@teliacompany.com>
2024-12-11 00:22:34 +0100

    Fix formatting of create_cmd_triggers in cluster credentials module

--- credentials.tf
+++ credentials.tf
@@ -4,7 +4,7 @@ module "cluster_credentials" {
   platform                          = var.platform
   additional_components             = ["kubectl", "gke-gcloud-auth-plugin", "beta"]
   create_cmd_body                   = "container hub memberships get-credentials ${google_gkeonprem_vmware_cluster.cluster.name} --project ${var.project_id} --quiet"
-  create_cmd_triggers = {var.connect_gateway_users }
+  create_cmd_triggers               = var.connect_gateway_users
   module_depends_on                 = [google_gkeonprem_vmware_cluster.cluster]
   use_tf_google_credentials_env_var = true
   activate_service_account          = true
