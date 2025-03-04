cba2b8c74c3b0b849d11c5eb320c6a83f801d537
SadriG91 <74971461+SadriG91@users.noreply.github.com>
2025-03-04 19:38:42 +0100

    Implement Helm chart for RBAC resources instead of terraform resources

--- providers.tf
+++ providers.tf
@@ -19,12 +19,14 @@ terraform {
   required_version = ">= 1.7.0"
 }
 
-provider "kubernetes" {
-  host  = local.connect_gateway_endpoint
-  token = data.google_client_config.provider.access_token
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "gke-gcloud-auth-plugin"
+provider "helm" {
+  kubernetes {
+    host  = local.connect_gateway_endpoint
+    token = data.google_client_config.provider.access_token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "gke-gcloud-auth-plugin"
+    }
   }
 }
 
