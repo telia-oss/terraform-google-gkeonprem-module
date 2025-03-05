dbaed691893bd346701dbc7b76ed38c8b47115c4
SadriG91 <74971461+SadriG91@users.noreply.github.com>
2025-03-05 11:06:35 +0100

    Fix Helm release chart path and update set_list syntax for connectGatewayUsers

--- connect-gateway-rbac.tf
+++ connect-gateway-rbac.tf
@@ -1,13 +1,13 @@
 resource "helm_release" "connect_gateway_rbac" {
   name      = "connect-gateway-rbac"
   namespace = "gke-connect"
-  chart     = "./connect-gateway-rbac"
+  chart     = "${path.module}/connect-gateway-rbac"
   wait      = true
   timeout   = 300
-  set_list {
+  set_list = [{
     name  = "connectGatewayUsers"
     value = var.connect_gateway_users
-  }
+  }]
   depends_on = [google_gkeonprem_vmware_cluster.cluster]
 }
 
