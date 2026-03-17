e5dd4e65ad0d9a6d2e43e7e7e4d4d76d788d9b83
SadriG91 <74971461+SadriG91@users.noreply.github.com>
2026-03-17 10:16:37 +0100

    Add Kubernetes provider to Terraform configuration

--- providers.tf
+++ providers.tf
@@ -15,6 +15,12 @@ terraform {
       source  = "hashicorp/helm"
       version = "3.0.0-pre2"
     }
+   kubernetes = { 
+      source = "hashicorp/kubernetes" 
+      version = "3.0.1"
+   }
+ }
+}
   }
   required_version = ">= 1.7.0"
 }
